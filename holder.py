314947848edc52337a6edaec0df2486f8a92b7d4
ofido <ofirfifi@gmail.com>
2019-07-20 01:08:30 +0300

    Added start delay to the holder

--- holder.py
+++ holder.py
@@ -1,13 +1,19 @@
 import mouse
 import keyboard
+import time
 
 BUTTONS = ['left', 'right']  # valid button names
+START_DELAY = 5
 
 
 def main():
 	button = input('Enter right or left: ')
 
 	if button in BUTTONS:
+		print('Will start in:')
+		for i in range(START_DELAY, 0, -1):  # count START_DELAY seconds and start
+			print(i)
+			time.sleep(1)
 		mouse.press(button=button)  # hold the button
 
 		print('Waiting for escape key in order to stop...')
